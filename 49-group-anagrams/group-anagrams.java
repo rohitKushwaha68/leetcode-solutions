cf53bea71c47124107a7c78d09dc4202f917af88
rohitKushwaha68 <69788796+rohitKushwaha68@users.noreply.github.com>
2026-09-25 16:57:44 +0530

    Time: 8 ms (34.67%) | Memory: 49.9 MB (26.99%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,54 +1,54 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
 
-    // List<List<String>> ans= new ArrayList<>();    
+    List<List<String>> ans= new ArrayList<>();    
     Map<String,List<String>> map= new HashMap<>();
 
-    // for(int i=0;i<strs.length;++i){
+    for(int i=0;i<strs.length;++i){
 
-    //     char curr[]=strs[i].toCharArray();
-    //     Arrays.sort(curr);
-    //     String currStr= new String(curr);
+        char curr[]=strs[i].toCharArray();
+        Arrays.sort(curr);
+        String currStr= new String(curr);
         
-    //     if(map.containsKey(currStr)){
-    //        map.get(currStr).add(strs[i]);
-    //     }else{
-    //          ArrayList<String> list = new ArrayList<>();
-    //             list.add(strs[i]);
-    //             map.put(currStr, list);
-    //     }
-    // }
-      
-    //   for(Map.Entry<String,List<String>> entry: map.entrySet()){
-    //       ans.add(entry.getValue());
-    //   }
-
-    //    return ans;
-
-      for(String str : strs){
-
-        int freq[]= new int[26];
-
-        for(char ch :str.toCharArray()){
-            ++freq[ch-'a'];
-        }
-
-         StringBuilder keyBuilder = new StringBuilder();
-
-            for (int count : freq) {
-                keyBuilder.append('#').append(count);
-            }
-         
-         String key = keyBuilder.toString();
-
-        if(map.containsKey(key)){
-           map.get(key).add(str);
+        if(map.containsKey(currStr)){
+           map.get(currStr).add(strs[i]);
         }else{
              ArrayList<String> list = new ArrayList<>();
-                list.add(str);
-                map.put(key, list);
+                list.add(strs[i]);
+                map.put(currStr, list);
         }
+    }
+      
+      for(Map.Entry<String,List<String>> entry: map.entrySet()){
+          ans.add(entry.getValue());
       }
-      return new ArrayList<>(map.values());
+
+       return ans;
+
+    //   for(String str : strs){
+
+    //     int freq[]= new int[26];
+
+    //     for(char ch :str.toCharArray()){
+    //         ++freq[ch-'a'];
+    //     }
+
+    //      StringBuilder keyBuilder = new StringBuilder();
+
+    //         for (int count : freq) {
+    //             keyBuilder.append('#').append(count);
+    //         }
+         
+    //      String key = keyBuilder.toString();
+
+    //     if(map.containsKey(key)){
+    //        map.get(key).add(str);
+    //     }else{
+    //          ArrayList<String> list = new ArrayList<>();
+    //             list.add(str);
+    //             map.put(key, list);
+    //     }
+    //   }
+    //   return new ArrayList<>(map.values());
     }
 }
