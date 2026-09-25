991de33b0916f4b126923f08d880a995aada898b
rohitKushwaha68 <69788796+rohitKushwaha68@users.noreply.github.com>
2026-09-25 15:55:14 +0530

    Time: 2 ms (99.61%) | Memory: 44.7 MB (55.42%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -4,20 +4,36 @@ class Solution {
         if(s.length()!=t.length())
            return false;
 
-        Map<Character,Integer> map1= new HashMap<>();
-        Map<Character,Integer> map2= new HashMap<>(); 
+        // Map<Character,Integer> map1= new HashMap<>();
+        // Map<Character,Integer> map2= new HashMap<>(); 
 
-          for(char ch: s.toCharArray()){
-            map1.put(ch,map1.getOrDefault(ch,0)+1);
-          }
+        //   for(char ch: s.toCharArray()){
+        //     map1.put(ch,map1.getOrDefault(ch,0)+1);
+        //   }
 
-          for(char ch: t.toCharArray()){
-            map2.put(ch,map2.getOrDefault(ch,0)+1);
-          }
+        //   for(char ch: t.toCharArray()){
+        //     map2.put(ch,map2.getOrDefault(ch,0)+1);
+        //   }
 
-          if(map1.equals(map2))
-            return true;
-           else
-           return false; 
+        //   if(map1.equals(map2))
+        //     return true;
+        //    else
+        //    return false;
+
+       int charFre[]= new int[26];
+
+       for(char ch:s.toCharArray()){
+         charFre[ch-'a']=charFre[ch-'a']+1;
+       }
+
+        for(char ch:t.toCharArray()){
+         charFre[ch-'a']=charFre[ch-'a']-1;
+       }
+
+       for(int i=0;i<charFre.length;++i){
+             if(charFre[i]!=0)
+              return false;
+       }
+         return true;
     }
 }
