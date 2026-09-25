5c79164603a36bb9dc4bfa7dcb3f20ce6d5a2481
rohitKushwaha68 <69788796+rohitKushwaha68@users.noreply.github.com>
2026-09-25 15:50:01 +0530

    Time: 13 ms (28.29%) | Memory: 46.5 MB (33.12%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        if(s.length()!=t.length())
+           return false;
+
+        Map<Character,Integer> map1= new HashMap<>();
+        Map<Character,Integer> map2= new HashMap<>(); 
+
+          for(char ch: s.toCharArray()){
+            map1.put(ch,map1.getOrDefault(ch,0)+1);
+          }
+
+          for(char ch: t.toCharArray()){
+            map2.put(ch,map2.getOrDefault(ch,0)+1);
+          }
+
+          if(map1.equals(map2))
+            return true;
+           else
+           return false; 
+    }
+}
